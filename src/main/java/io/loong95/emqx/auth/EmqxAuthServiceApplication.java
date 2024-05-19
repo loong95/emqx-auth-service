3166f31136a8653270f42c382bb1095c78cc0ad6
linyunlong <yunlonglin1995@gmail.com>
2024-05-19 17:59:49 +0800

    初始化项目

---
+++ src/main/java/io/loong95/emqx/auth/EmqxAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package io.loong95.emqx.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmqxAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmqxAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/loong95/emqx/auth/EmqxAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.loong95.emqx.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmqxAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
